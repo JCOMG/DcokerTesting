592ce39ebbf572aa900a6bddc19fa888b0941922
JCOMG <CXC1217@student.bham.ac.uk>
2024-10-10 15:13:46 +0100

    modify ok

--- TESTDocker/app/app.py
+++ TESTDocker/app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "okkkkkkk"
+    return "okkkkkkk!!!!!!!!!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
